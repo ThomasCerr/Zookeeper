f6ff21112e57f50e41152510fd6901a1310ba281
ThomasCerr <99366289+ThomasCerr@users.noreply.github.com>
2022-05-03 16:04:21 -0400

    l1

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
@@ -41,14 +42,25 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
     }
-    res.json(results);
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      res.json(result);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
